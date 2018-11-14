ba5a3d9e83a9d1bbd6016578a82f97b7cce629c6
Dwi Teguh Prasetyo <dtprsty@gmail.com>
2018-11-14 21:18:14 +0700

    revisi product_review

--- db/migrate/20181114095616_create_product_reviews.rb
+++ db/migrate/20181114095616_create_product_reviews.rb
@@ -1,14 +1,10 @@
 class CreateProductReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :product_reviews do |t|
-      t.string :product_id
-      t.string :int
-      t.string :user_id
-      t.string :int
+      t.integer :product_id
+      t.integer :user_id
       t.string :review
-      t.string :string
-      t.string :rate
-      t.string :int
+      t.integer :rate
 
       t.timestamps
     end
