e54aa45653e3934340373790addc52e865fb5601
riswantisiallagan <riswantisiallagan1702@gmail.com>
2018-11-15 15:13:38 +0700

    add user_detail

---
+++ db/migrate/20181115071732_create_user_details.rb
@@ -0,0 +1,13 @@
+class CreateUserDetails < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_details do |t|
+      t.text :fullname
+      t.text :addres
+      t.integer :gender
+      t.varchar :phone_number
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
