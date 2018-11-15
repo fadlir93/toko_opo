05cf45f909b63c136b836af0ff50b63c64ed35a5
blacksyan <43898251+blacksyan@users.noreply.github.com>
2018-11-15 07:01:12 +0700

    Feature/product (#9)
    * Create model product
    
    * feature/order

---
+++ db/migrate/20181114134837_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.integer :user_id
+      t.date :order_date
+      t.date :shipping_id
+
+      t.timestamps
+    end
+  end
+end
