ee8715ccd29e467546b7c1deba7669c8119715e4
rhizalfahmi <rhizall.00@gmail.com>
2018-11-15 09:59:19 +0700

    update

--- db/migrate/20181114233613_create_users.rb
+++ db/migrate/20181114233613_create_users.rb
@@ -1,18 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.primary_key "user_id"
       t.string "ulogin"
       t.string "upass"
       t.string "username"
       t.string "uemail"
-      t.text "fullname"
-      t.integer "phonenumber"
-      t.string "ustatus"
-      t.string "ugender"
-      t.string "urating"
-      t.text "address"
-      t.text "ubio"
       t.timestamps
     end
   end
