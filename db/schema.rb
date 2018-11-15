817c4c05679ce604e98b9100535f01f61ad9e24a
mhpranajaya <mhpranajaya@gmail.com>
2018-11-15 16:29:34 +0700

    Make relation with Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_233613) do
+ActiveRecord::Schema.define(version: 2018_11_15_080925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "products", force: :cascade do |t|
-    t.text :name
-    t.text :description
-    t.float :price
-    t.integer :stock
-    t.timestamps
-  end
-
   create_table "categories", force: :cascade do |t|
-
     t.text "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.string "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.text "name"
+    t.text "description"
+    t.float "price"
+    t.integer "stock"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
