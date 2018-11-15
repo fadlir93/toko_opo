09957f005ac5efe578c48528554566eda0eead45
rhizalfahmi <rhizall.00@gmail.com>
2018-11-15 13:22:08 +0700

    rizal-create user (#10)
    * create user
    
    * update
    
    * user and email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_093851) do
+ActiveRecord::Schema.define(version: 2018_11_14_233613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2018_11_14_093851) do
     t.text "description"
     t.float "price"
     t.integer "stock"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "ulogin"
+    t.string "upass"
+    t.string "username"
+    t.string "uemail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
