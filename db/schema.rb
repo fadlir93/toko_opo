2e3120f47a56d79da090514e652907c1930a6c45
Dwi Teguh Prasetyo <dtprsty@gmail.com>
2018-11-15 16:33:52 +0700

    upload product_review (#3)
    * upload product_review
    
    * revisi product_review
    
    * upload product_review

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_083328) do
+ActiveRecord::Schema.define(version: 2018_11_15_090543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "products", force: :cascade do |t|
-    t.text :name
-    t.text :description
-    t.float :price
-    t.integer :stock
-    t.timestamps
+  create_table "categories", force: :cascade do |t|
+    t.text "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_reviews", force: :cascade do |t|
+    t.bigint "product_id"
+    t.bigint "user_id"
+    t.string "review"
+    t.integer "rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_product_reviews_on_product_id"
+    t.index ["user_id"], name: "index_product_reviews_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -54,4 +64,6 @@ ActiveRecord::Schema.define(version: 2018_11_15_083328) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "product_reviews", "products"
+  add_foreign_key "product_reviews", "users"
 end
