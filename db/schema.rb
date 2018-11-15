4506b08c2949df04957ab9d27f062f0a3fd636a5
fadlir93 <42757657+fadlir93@users.noreply.github.com>
2018-11-15 14:47:12 +0700

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,22 +15,6 @@ ActiveRecord::Schema.define(version: 2018_11_14_233613) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "ads", force: :cascade do |t|
-    t.integer "user_id"
-    t.date "order_date"
-    t.date "shipping_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.integer "user_id"
-    t.date "order_date"
-    t.date "shipping_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "products", force: :cascade do |t|
     t.text :name
     t.text :description
