e5277d6c6fdcd0d3a9aac50656008e0bb9954ef1
blacksyan <43898251+blacksyan@users.noreply.github.com>
2018-11-16 16:23:37 +0700

    Feature/product --add controller with API (#19)
    * Create model product
    
    * feature/order
    
    * restore product model
    
    * Delete order.rb
    
    * Delete ad.rb
    
    * Delete 20181114134837_create_orders.rb
    
    * Delete 20181114134325_create_ads.rb
    
    * Update schema.rb
    
    * Delete ads.yml
    
    * Delete orders.yml
    
    * Delete order_test.rb
    
    * Delete ad_test.rb
    
    * add controller with API

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get "/product", to: "product#index"
+  get "/product/detail", to: "product#show"
+  delete "/product/delete", to: "product#destroy"
+  post "/product/new", to: "product#create"
+
+
   
+
 
 end
