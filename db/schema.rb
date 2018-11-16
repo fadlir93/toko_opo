8c7767a0307203e264b422618091620d73e6ebeb
clive <francoclivemaleke23@gmail.com>
2018-11-16 15:42:27 +0700

    fixed product category

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_11_15_035148) do
+ActiveRecord::Schema.define(version: 2018_11_16_082924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ads", force: :cascade do |t|
+    t.integer "user_id"
+    t.date "order_date"
+    t.date "shipping_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.text "name"
@@ -30,8 +37,8 @@ ActiveRecord::Schema.define(version: 2018_11_15_035148) do
     t.string "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_product_reviews_on_product_id"
-    t.index ["user_id"], name: "index_product_reviews_on_user_id"
+    t.index ["order_id"], name: "index_order_details_on_order_id"
+    t.index ["product_id"], name: "index_order_details_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -43,20 +50,23 @@ ActiveRecord::Schema.define(version: 2018_11_15_035148) do
   end
 
   create_table "product_categories", force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "category_id"
+    t.bigint "product_id"
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.string "order_date"
+  create_table "product_reviews", force: :cascade do |t|
+    t.bigint "product_id"
     t.bigint "user_id"
     t.string "review"
     t.integer "rate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_orders_on_user_id"
+    t.index ["product_id"], name: "index_product_reviews_on_product_id"
+    t.index ["user_id"], name: "index_product_reviews_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -78,6 +88,8 @@ ActiveRecord::Schema.define(version: 2018_11_15_035148) do
   add_foreign_key "order_details", "orders"
   add_foreign_key "order_details", "products"
   add_foreign_key "orders", "users"
+  add_foreign_key "product_categories", "categories"
+  add_foreign_key "product_categories", "products"
   add_foreign_key "product_reviews", "products"
   add_foreign_key "product_reviews", "users"
 end
