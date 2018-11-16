a9c875acf22d740c39da2f69be4d4b8bd1666435
cliveaja <39453434+cliveaja@users.noreply.github.com>
2018-11-16 09:56:08 +0700

    adding product_category (#4)
    * Create model product
    
    * adding product_category
    
    * tested product category
    
    * Update schema.rb
    
    Table Order ada 2 jadi yang 1 saya hapus

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_090543) do
+
+ActiveRecord::Schema.define(version: 2018_11_15_035148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +37,13 @@ ActiveRecord::Schema.define(version: 2018_11_15_090543) do
   create_table "categories", force: :cascade do |t|
     t.text "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
