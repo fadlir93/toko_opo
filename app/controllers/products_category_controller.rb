f5ae6a893ddbf5c78eef971f794c37f1468825bf
clive <francoclivemaleke23@gmail.com>
2018-11-19 21:15:02 +0700

    adding API

--- app/controllers/products_category_controller.rb
+++ app/controllers/products_category_controller.rb
@@ -1,15 +1,85 @@
 class ProductsCategoryController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
-    def show
-      products_category = ProductCategory.select
-        if products_category.present?
-          render json: products_category, status: :ok
-        else
-          render json: {
-            message: "data not found"
-          }, status: :unprocessable_entity
-        end
+=begin
+  memanggil method find_product_category_id sebelum memanggil 
+  method destroy dan show
+=end
+  before_action :find_product_category_id, only: [:destroy, :show]
 
+  def index
+    # memasukkan semua data ke instance variabel @product_category 
+    # dari model ProductCategory
+    @product_category = ProductCategory.all()
+
+    # conditional, jika ada data pada @product_category maka, data tampil dalam
+    # bentuk json, jika tidak ada maka muncul pesan Product Category not found
+    if @product_category.present?
+      render json: { 
+        status: "ok", 
+        data: @product_category, 
+        error: nil
+      }, status: :ok
+    else
+      render json: {
+        message: "Product Category not found"
+      }, status: :unprocessable_entity
     end
+    # end coditional
+  end
+
+  def show
+    if @product_category.present?
+      render json: { 
+        status: "ok", 
+        data: @product_category, 
+        error: nil
+      }, status: :ok
+    else
+      render json: {
+        message: "Product Category not found"
+      }, status: :unprocessable_entity
+    end
+
+  end
+
+  def destroy
+    if @product_category
+      @product_category.destroy!
+      render json: {
+        message: "Delete Success"
+      }, status: :ok
+    else
+      render json: {
+        message: "Delete Failed"
+      }, status: :unprocessable_entity
+    end
+
+  end
+
+  def create
+    create_product = ProductCategory.new(product_params)
+    if create_product.save
+      render json: {
+        message: "Data Inserted"
+      }, status: :created
+    else
+      render json: {
+        message: "Insert data failed"
+      }, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+  def product_params
+    params.require(:product_category).permit(:product_id, :category_id)
+    # params.permit(:product_id, :category_id)
+
+  end
+
+  def find_product_category_id
+    @product_category = ProductCategory.find_by(id: params[:id])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  
+  get "/products_category", to: "products_category#index"
+=begin 
+  get : -> nama url tujuan, to: -> nama_controller#methodnya, as: -> namapath 
+  yang fungsinya untuk dipanggil di controller,helper maupun view, 
+  contohnya di view <%= link_to show_path, "click here to show" %>
+=end
+  get "/product_category/detail", to: "products_category#show", as: "show"
+  delete "/product_category/delete", to: "products_category#destroy"
+  post "/product_category/create", to: "products_category#create"
 
 end
