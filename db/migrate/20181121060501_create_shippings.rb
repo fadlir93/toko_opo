10b317b18a9ae09738116724d4c00b28f7b207da
Angga Febri Eka Susani <angga.febrieka@gmail.com>
2018-11-23 09:56:24 +0700

    Tabel shipping dan isi

--- db/migrate/20181121060501_create_shippings.rb
+++ db/migrate/20181121060501_create_shippings.rb
@@ -3,8 +3,7 @@ class CreateShippings < ActiveRecord::Migration[5.2]
     create_table :shippings do |t|
       t.text :name
       t.text :description
-      t.refrences :order
-
+      
       t.timestamps
     end
   end
